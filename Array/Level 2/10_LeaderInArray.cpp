5b91b23deca1baf24d51b852a1270c2bcf84073e
BaalNathi <taherbatterywala1105@gmail.com>
2025-11-14 11:01:28 +0530

    Leader in array

---
+++ Array/Level 2/10_LeaderInArray.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the length of an Array - ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of an Array - ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Array created - ";  
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int maxi = INT_MIN;
+    vector<int> ans;
+    for(int i = n-1; i>=0; i--){
+        if(arr[i]>maxi){
+            ans.push_back(arr[i]);
+        }
+        maxi = max(maxi,arr[i]);
+    }
+    cout<<"The leaders in the array are - ";
+    for(int i = 0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+}
