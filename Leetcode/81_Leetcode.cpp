1f15f72e78df4dfeca8e8fff86612734a9137f4e
BaalNathi <taherbatterywala1105@gmail.com>
2025-11-24 19:31:09 +0530

    Leetcode 81

---
+++ Leetcode/81_Leetcode.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target) return true;
+            if(nums[mid]==nums[start] && nums[mid]==nums[end]){
+                start++;
+                end--;
+                continue;
+            }
+            if(nums[mid] >= nums[start]){
+                if(target >= nums[start] && target<nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target>nums[mid] && nums[end]>=target){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
