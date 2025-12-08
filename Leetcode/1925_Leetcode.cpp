a7cb552ed730af82ad559f0c52a87c1d242ac7e5
BaalNathi <taherbatterywala1105@gmail.com>
2025-12-08 13:02:03 +0530

    Leetcode 1925

---
+++ Leetcode/1925_Leetcode.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countTriples(int n) {
+        int cnt = 0;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                int sum = (i*i) + (j*j);
+                int c = sqrt(sum);
+                if(c*c == sum && c<=n){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
