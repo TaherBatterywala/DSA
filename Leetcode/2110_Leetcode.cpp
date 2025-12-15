5d20f41a0f642919bf6d304ffe17b8c7c7426e98
BaalNathi <taherbatterywala1105@gmail.com>
2025-12-15 17:21:52 +0530

    2110 Leetcode

---
+++ Leetcode/2110_Leetcode.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0;
+        int cnt = 1;      
+        for(int i = 0; i < prices.size(); i++){
+            if(i == 0){
+                ans += cnt;
+                continue;
+            }
+            if(prices[i] == prices[i-1] - 1){
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            ans += cnt;
+        }
+        
+        return ans;
+    }
+};
