04e9f470b564d85f6cdffdf723139bc8d00057d2
BaalNathi <taherbatterywala1105@gmail.com>
2026-01-02 19:24:04 +0530

    Leetcode 3158 1486

---
+++ Leetcode/3158_Leetcode.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) {
+        unordered_map<int, int> m;
+        int xorr = 0;
+        for (int i : nums) {
+            if (m[i]) xorr ^= i;
+            m[i]++;
+        }
+        return xorr;
+    }
+};
