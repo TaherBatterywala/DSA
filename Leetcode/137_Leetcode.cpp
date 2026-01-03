093239fc5513839702856b0d78479cdb4dfa2735
BaalNathi <taherbatterywala1105@gmail.com>
2026-01-03 19:05:48 +0530

    Leetcode daily - 1441,137

---
+++ Leetcode/137_Leetcode.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-1;i+=3){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[n-1];
+    }
+};
